261bcd93cab1cc47397231e9504442de2cb450b1
Harry Parkes <harrydparkes@protonmail.com>
2020-06-08 07:07:50 +1200

    Totals scores on refresh

--- sum.js
+++ sum.js
@@ -1,11 +1,23 @@
 var playerCount = 8;
 
 table = [];
+function init() {   
+    for(var X = 0; X < playerCount; X++) {
 
-for(var X = 0; X < playerCount; X++) {
-    table[X] = [];
-    for(var Y = 0; Y < 10; Y++) {
-        table[X][Y] = 0
+        table[X] = [];
+        
+        for(var Y = 0; Y < 10; Y++) {
+
+            var elmntStr = 'cell_'+String.fromCharCode(X+65)+String(Y+1)
+            var elmnt = document.getElementById(elmntStr);
+
+            if(elmnt.value == null)
+                table[X][Y] = 0;
+            else
+                table[X][Y] = elmnt.value;
+            
+            sum(elmntStr);
+        }
     }
 }
 
@@ -13,7 +25,7 @@ function sum(element) {
     var elmnt = document.getElementById(element);
     var row = Number(element.slice(6))-1;
     var coll = element.slice(5,6);
-    var col = element.slice(5,6).charCodeAt()-65;
+    var col = coll.charCodeAt()-65;
     table[col][row] = elmnt.value;
 
     total = 0;
@@ -21,5 +33,5 @@ function sum(element) {
     for(var cell = 0; cell < table[col].length; cell++) {
         total += Number(table[col][cell]);
     }
-    document.getElementById('cell_total'+coll).innerHTML = total;
+    document.getElementById('cell_total'+coll).innerText = total;
 }
